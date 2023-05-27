a94cbbfb3c4bdb95d5db5c2627831abcf4bc647c
happyday <jsoibn@gmail.com>
2023-05-28 03:03:00 +0700

    install tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from "./logo.svg"
 import "./App.css"
-import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <h1 className="text-3xl font-bold underline">Test</h1>
     </div>
   )
 }
