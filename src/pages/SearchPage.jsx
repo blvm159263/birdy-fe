02fcc2d49fd565f8250836cba841e80573cc187f
Tam Long <aqua.tamlong@gmail.com>
2023-06-01 11:49:53 +0700

    Remove old link items

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -8,16 +8,7 @@ import StoreCard from '../components/store/StoreCard'
 export default function SearchPage() {
 
   return (
-    <div id='searchPage' className='bg-neutral-100 px-2 md:px-0'>
-      <div className='flex justify-center py-6'>
-        <Link to="/search" className='text-sm md:text-xl font-bold uppercase active text-sky-500'>All</Link>
-        <span className='text-neutral-400 text-sm md:text-xl font-bold mx-2 md:mx-8'>/</span>
-        <Link to="/search" className='text-sm md:text-xl font-bold uppercase'>Bird</Link>
-        <span className='text-neutral-400 text-sm md:text-xl font-bold mx-2 md:mx-8'>/</span>
-        <Link to="/search" className='text-sm md:text-xl font-bold uppercase'>Accessories</Link>
-        <span className='text-neutral-400 text-sm md:text-xl font-bold mx-2 md:mx-8'>/</span>
-        <Link to="/search" className='text-sm md:text-xl font-bold uppercase'>Decor</Link>
-      </div>
+    <div id='searchPage' className='bg-neutral-100 px-2 md:px-0 py-4'>
       <section className='container mx-auto'>
         <div className='flex justify-between mb-4'>
           <p className='text-neutral-500'>Shop related to “<span className='text-orange-500'>cage</span>”</p>
