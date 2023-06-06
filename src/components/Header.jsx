12a69df98790920f4bde5df97df46cf629960088
Tam Long <aqua.tamlong@gmail.com>
2023-06-06 16:53:00 +0700

    Improve homepage ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,12 +12,12 @@ export default function NavBar() {
 
   return (
     <nav className="bg-gradient-to-r from-sky-500 via-blue-500 to-sky-500 border-gray-200">
-      <div className="container flex flex-wrap items-center justify-between mx-auto p-3 md:p-8 relative">
+      <div className="container flex flex-wrap items-center justify-between mx-auto p-3 md:px-8 md:py-6 lg:py-4 relative">
         <Link to="/" className="flex items-center">
-          <img src="/assets/images/logo-white.png" className="h-12 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Birdy</span>
+          <img src="/assets/images/logo-white.png" className="h-8 md:h-12 mr-3" alt="Birdy Logo" />
+          <span className="self-center text-xl md:text-3xl lg:text-4xl font-semibold whitespace-nowrap dark:text-white">Birdy</span>
         </Link>
-        <div className="flex flex-col">
+        <div className="flex flex-col md:pr-16 lg:pr-24">
           <SearchBar/>
           <ul className='hidden md:flex text-white justify-between pt-2'>
             <li><Link to={`/search/${SearchType.ALL_PRODUCT.text}/0`}>All Products</Link></li>
@@ -34,13 +34,7 @@ export default function NavBar() {
         </div>
         
         <div className={`${showMobileMenu ? "" : "hidden"} md:hidden mobile-menu items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-search">
-          <div className="relative mt-3 md:hidden">
-            <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-              <svg className="w-5 h-5 text-neutral-600" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-              <span className="sr-only">Search icon</span>
-            </div>
-            <input type="text" id="search-navbar" className="block w-full p-2 pr-10 text-sm border text-neutral-700 border-neutral-600 rounded-sm bg-sky-500 placeholder-neutral-100 outline-sky-500 ring-black" placeholder="Search on Birdy"/>
-          </div>
+          <SearchBar forMobile/>
           <ul className="flex flex-col p-2 mt-4 gap-4 font-semibold text-white uppercase">
             <li><Link to={`/search/${SearchType.ALL_PRODUCT.text}/0`}>All Products</Link></li>
             <li><Link to={`/search/${SearchType.BIRD.text}/0`}>Birds</Link></li>
@@ -53,8 +47,8 @@ export default function NavBar() {
           </ul>
         </div>
 
-        <div className='absolute top-0 right-0 hidden md:flex text-neutral-800'>
-          <Link to="/" className='px-2 pt-1 mr-4'>SELL PRODUCT</Link>
+        <div className='absolute top-0 right-0 hidden md:flex text-white text-sm xl:text-base'>
+          <Link to="/" className='px-2 pt-1 mr-2'>SELL PRODUCT</Link>
           <Link to="/login" className='px-2 pt-1'>SIGN IN</Link>
           <span className='px-2 pt-[0.2rem]'>|</span>
           <Link to="/" className='px-2 pt-1'>SIGN UP</Link>
