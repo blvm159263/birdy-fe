83b422a4ae5a9de8bfa4c6e2e8dd81d664996dd1
Tam Long <aqua.tamlong@gmail.com>
2023-06-11 16:10:37 +0700

    clean logging

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -16,14 +16,10 @@ const cartSlice = createSlice({
             } else {
                 state.items.push({ ...action.payload, quantity: action.payload.quantity, shopId: action.payload.shopId });
             }
-            console.log('Add item ' + action.payload.id + ' to cart!');
-            console.log(state.items);
         },
         incrementQuantity: (state, action) => {
             const item = state.items.find((item) => item.id === action.payload.id);
             item.quantity += action.payload.quantity;
-            console.log('incrementQuantity for item id ' + action.payload);
-            console.log(state.items);
         },
         decrementQuantity: (state, action) => {
             const item = state.items.find((item) => item.id === action.payload);
@@ -32,14 +28,10 @@ const cartSlice = createSlice({
             } else {
                 item.quantity--;
             }
-            console.log('decrementQuantity for item id ' + action.payload);
-            console.log(state.items);
         },
         removeItem: (state, action) => {
             const nonRemoveItems = state.items.filter((item) => item.id !== action.payload);
             state.items = nonRemoveItems;
-            console.log('removeItem id' + action.payload);
-            console.log(state.items);
         }
     }
 });
