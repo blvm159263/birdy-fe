e87443cacba49ff7ab5d9411cd6feec585b68810
n-d-h <duchoang4195@gmail.com>
2023-06-14 17:22:05 +0700

    update shop management ui

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -7,22 +7,22 @@ const productApi = {
     },
     getAll(params) {
         let url = '/products/view'
-        if(params.id !== undefined) url = '/categories/' + params.id + url;
+        if (params.id !== undefined) url = '/categories/' + params.id + url;
         return axiosClient.get(url, { params })
     },
     getAllAscending(params) {
         let url = '/products/view/price-asc'
-        if(params.id !== undefined) url = '/categories/' + params.id + url;
+        if (params.id !== undefined) url = '/categories/' + params.id + url;
         return axiosClient.get(url, { params })
     },
     getAllDescending(params) {
         let url = '/products/view/price-desc'
-        if(params.id !== undefined) url = '/categories/' + params.id + url;
+        if (params.id !== undefined) url = '/categories/' + params.id + url;
         return axiosClient.get(url, { params })
     },
     getAllLatest(params) {
         let url = '/products/view/latest'
-        if(params.id !== undefined) url = '/categories/' + params.id + url;
+        if (params.id !== undefined) url = '/categories/' + params.id + url;
         return axiosClient.get(url, { params })
     },
     getProductById(id) {
@@ -30,9 +30,47 @@ const productApi = {
         return axiosClient.get(url);
     },
     addNewProduct(params) {
+        const data = new FormData();
+        data.append('productDTO', params.productDTO);
+        data.append('mainImage', params.mainImage);
+        params.subImages.forEach((image) => {
+            data.append('subImages', image);
+        });        
+        const config = {
+            headers: {
+                'Content-type': 'multipart/form-data',
+            },
+        };
         const url = '/products/create'
-        return axiosClient.post(url, params);
+        return axiosClient.post(url, data, config);
     },
+    // test(params) {
+    //     const data = new FormData();
+    //     data.append('mainImage', params);
+    //     const config = {
+    //         headers: {
+    //             'Content-type': 'multipart/form-data',
+    //         },
+    //     };
+    //     const url = '/products/test'
+    //     return axiosClient.post(url, data, config);
+    // }
+    test(params) {
+        const data = new FormData();
+        data.append('productDTO', params.productDTO);
+        data.append('mainImage', params.mainImage);
+        params.subImages.forEach((image) => {
+            data.append('subImages', image);
+        });        
+        const config = {
+            headers: {
+                'Content-type': 'multipart/form-data',
+            },
+        };
+        const url = '/products/test'
+        return axiosClient.post(url, data, config);
+    }
+
 };
 
 export default productApi;
