11c82cb774d8b369e8f47db3f0dbab76f50010e8
Tam Long <aqua.tamlong@gmail.com>
2023-07-11 06:23:19 +0700

    Add view report modal

--- src/components/admin/product-reports/ProductReportModal.jsx
+++ src/components/admin/product-reports/ProductReportModal.jsx
@@ -1,30 +1,80 @@
-import { Modal } from "antd";
+import { WarningOutlined } from "@ant-design/icons";
+import { Modal, Spin } from "antd";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import adminApi from "../../../api/adminApi";
 import { closeProductReportModal } from "../../../features/admin/adminSlice";
-import DetailItemPage from "../../../pages/DetailItemPage";
+import ProductDetails from "../../detail-page/ProductDetails";
 import ProductOverview from "../../detail-page/ProductOverview/ProductOverview";
+import Review from "../../detail-page/Review";
 
 export default function ProductReportModal() {
   const isModalOpen = useSelector(
     (state) => state.admin.isProductReportModalOpen
   );
   const report = useSelector((state) => state.admin.selectedProductReport);
-  const {product} = report ? report : {};
+  const [reportDetail, setReportDetail] = useState(null);
+  const { product } = report ? report : {};
   const dispatch = useDispatch();
 
   const onCancel = () => {
     dispatch(closeProductReportModal());
+    setReportDetail(null);
   };
+
+  useEffect(() => {
+
+    if(report) {
+      adminApi.getReportsByProductId(report.product.id)
+      .then((response) => {
+        setReportDetail(response.data);
+        console.log(response.data)
+      })
+    }
+  }, [report])
 
   return (
     <Modal
-      title="Product details"
+      title="Report details"
       open={isModalOpen}
       onCancel={onCancel}
       footer={null}
+      width='80vw'
     >
       {report ? (
-        <ProductOverview product={product} />
+        <div className="grid grid-cols-12">
+          {/* Infos */}
+          <div className="col-span-8">
+            <ProductOverview product={product} noControl />
+            <div className="grid grid-cols-12 gap-2 mt-2">
+              <ProductDetails product={product} />
+              <Review product={product} />
+            </div>
+          </div>
+
+          {/* Reports */}
+          {reportDetail ? (
+            <div className="col-span-4 flex flex-col p-4 gap-2">
+              <h2 className={`font-bold text-xl ${report.reportCount > 3 ? 'text-red-500' : 'text-orange-400'}`}>Sản phẩm này có <span className="font-bold text-2xl">{report.reportCount}</span> lượt báo xấu   <WarningOutlined/></h2>
+              {reportDetail.report.map((report) => (
+                <div className="flex gap-4 items-center mt-4 pl-4">
+                  <p className={`font-bold text-lg ${report.reasonCount > 3 ? 'text-red-500' : 'text-orange-400'}`}>{report.reasonCount}</p>
+                  <p>{report.reason}</p>
+                </div>
+              ))}
+              <button className={`mt-6 h-12 w-full rounded shadow bg-gradient-to-r from-orange-500 to-orange-700 text-white font-bold hover:brightness-125 active:brightness-110 duration-150`}>
+                Gửi cảnh cáo
+              </button>
+              <button className={`h-12 w-full rounded shadow bg-gradient-to-r from-red-500 to-red-800 text-white font-bold hover:brightness-125 active:brightness-110 duration-150`}>
+                Xoá sản phẩm
+              </button>
+            </div>
+          ) : (
+            <div className='col-span-4 flex justify-center items-center h-[80vh]'>
+              <Spin />
+            </div>
+          )}
+        </div>
       ) : (
         "No report"
       )}
--- src/components/detail-page/ProductOverview/ProductOverview.jsx
+++ src/components/detail-page/ProductOverview/ProductOverview.jsx
@@ -2,11 +2,11 @@ import React from "react"
 import ProductOverviewImg from "./ProductOverviewImg"
 import ProductOverviewAction from "./ProductOverviewAction"
 
-function ProductOverview({ product }) {
+function ProductOverview({ product, noControl }) {
   return (
     <div className="bg-white rounded-md shadow grid grid-cols-12 gap-4 lg:gap-8 p-4 lg:p-8 mb-2">
       <ProductOverviewImg product={product} />
-      <ProductOverviewAction product={product} />
+      <ProductOverviewAction product={product} noControl={noControl}/>
     </div>
   )
 }
