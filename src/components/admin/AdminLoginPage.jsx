ac4458d52a8ce2d9a5bdd53c190c3eaf38c8765b
Tam Long <aqua.tamlong@gmail.com>
2023-07-11 17:16:00 +0700

    Add admin login + route authenticate

--- src/components/admin/AdminLoginPage.jsx
+++ src/components/admin/AdminLoginPage.jsx
@@ -1,24 +1,77 @@
+import { useDispatch } from "react-redux";
+import AdminHeader from "./AdminHeader";
+import { useState } from "react";
+import adminApi from "../../api/adminApi";
+import { saveAdminLoginInformation } from "../../features/admin/adminSlice";
+import { useEffect } from "react";
+
 export default function AdminLoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAuthenticating, setAuthenticating] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    setError(null);
+
+    if (username.trim().length === 0 || password.trim().length === 0) {
+      setError('Vui lòng điền tên đăng nhập và mật khẩu')
+      return;
+    }
+
+    // Authenticate
+    setAuthenticating(true);
+    adminApi.authenticate(username, password)
+      .then(response => {
+        dispatch(saveAdminLoginInformation({
+          username: username,
+          password: password
+        }))
+      }).catch((error) => {
+        setError('Tên đăng nhập hoặc mật khẩu không đúng')
+      }).finally(() => {
+        setAuthenticating(false);
+      })
+  }
+
+  useEffect(() => {
+    console.log(username + ' - ' + password);
+  }, [username, password])
+
   return (
-    <div className=" py-8 bg-gradient-to-r from-sky-500 via-blue-500 to-sky-500 border-gray-200 ">
-      <div className="container grid grid-cols-2 mx-auto">
+    <div className="h-screen bg-gradient-to-r from-sky-500 via-blue-500 to-sky-500">
+      <AdminHeader />
+      <div className="container grid grid-cols-1 md:grid-cols-2 mx-auto h-[80vh]">
+        {/* Left */}
         <div className="flex flex-col justify-center items-center">
-          <div className="h-12 mb-5">
+          <div className="flex flex-col gap-6 items-center">
             <img
               src="/assets/images/logo-white.png"
-              className="h-full"
-              alt="Flowbite Logo"
+              className="h-16"
+              alt="logo"
             />
+            <h1 className="text-white font-normal tracking-wide text-3xl">
+              Welcome to Birdy!
+              <br /> A bird trading platform
+            </h1>
           </div>
-          <h1 className="text-violet-600	 font-normal tracking-wide	 text-3xl text-center ">
-            Welcome to Birdy!
-            <br /> A bird trading platform
-          </h1>
-        </div>
-        <div>
-          form
         </div>
 
+        {/* Right */}
+        <div className="rounded shadow bg-white h-[450px] my-auto p-8">
+          <h1 className="text-2xl font-bold text-center">Đăng nhập tài khoản admin</h1>
+          <div className="mt-8">
+            <label className="block mb-1">Tên đăng nhập</label>
+            <input value={username} onChange={(e) => setUsername(e.target.value)} type='text' className="w-full rounded shadow bg-neutral-100 py-2 px-4 border border-neutral-300 outline-0" placeholder='Điền tên đăng nhập' />
+          </div>
+          <div className="mt-4">
+            <label className="block mb-1">Mật khẩu</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' className="w-full rounded shadow bg-neutral-100 py-2 px-4 border border-neutral-300 outline-0" placeholder='Điền mật khẩu' />
+          </div>
+          <p className={`text-red-500 text-xs mt-4 ${error ? 'opacity-100' : 'opacity-0'} duration-100`}>* {error}</p>
+          <button onClick={() => handleLogin()} className={`mt-4 h-12 w-full rounded shadow bg-gradient-to-r from-sky-500 to-sky-700 text-white font-bold hover:brightness-125 active:brightness-110 duration-150 ${isAuthenticating ? 'grayscale pointer-events-none hover:pointer-events-none' : ''}`}>{isAuthenticating ? 'Đang xử lý...' : 'Đăng nhập'}</button>
+        </div>
       </div>
     </div>
   )
--- src/components/admin/utils/ProtectedRoutes.js
+++ src/components/admin/utils/ProtectedRoutes.js
@@ -2,16 +2,23 @@ import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 import adminApi from "../../../api/adminApi";
 
-const ProtectedRoute = ({children}) => {
+const ProtectedRoutes = ({children}) => {
     const adminLoginInfomation = useSelector(state => state.admin.adminLoginInfomation)
     const {username, password} = adminLoginInfomation;
     let location = useLocation();
+
+    if(username === null || password === null) {
+        return <Navigate to="/admin/login" state={{from: location}} replace />
+    }
 
     // Authenticate
     adminApi.authenticate(username, password)
     .then(response => {
         return children
     }).catch((error) => {
+        console.log(error);
         return <Navigate to="/admin/login" state={{from: location}} replace />
     })
 }
+
+export default ProtectedRoutes
